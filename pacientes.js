cb17ce5f725403418913147f6fe86d4fecef2664
Marlon81785 <marlon81785@gmail.com>
2021-01-27 05:21:39 -0300

    opa :)

--- pacientes.js
+++ pacientes.js
@@ -1,30 +1,11 @@
 const pacients = [
     
     {
-        'nome': 'Filipe Iego Gomes',
-        'cpf': '012.507.386-01',
-        'dataNasc': '1988-03-25'
+        'nome': 'fulano de tal',
+        'cpf': '000.000.000-00',
+        'dataNasc': '0000-00-00'
     },
-    {
-        'nome': 'Fatima Conceicao dos santos',
-        'cpf': '789.217.916-04',
-        'dataNasc': '1962-07-14'
-    },
-    {
-        'nome': 'Gustavo Malaquias de oliveira',
-        'cpf': '097.311.416-93',
-        'dataNasc': '1993-11-19'
-    },
-    {
-        'nome': 'Agnaldo Mendes de oliveira',
-        'cpf': '114.772.936-04',
-        'dataNasc': '1987-04-24'
-    },
-    {
-        'nome': 'altamir da silva lage',
-        'cpf': '243.404.306-25',
-        'dataNasc': '1946-09-29'
-    },
+    
     
 
 
