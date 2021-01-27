6cac655f3ef0d3321b985f5db500f91515578cae
Marlon81785 <marlon81785@gmail.com>
2021-01-27 05:20:18 -0300

    tabela :)

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const url = 'https://mgapp.mg.gov.br/egov-servicos-web/rest/egov/listarBaseadoExemploEgov';
 var token = 'a1a95d2b5dc08aa0435f58deb6832c589c0e826bd642cf527bb8b2163e212230';
 
-
+var global = null
 
 async function sendRequest(nome, cpf, dataNascimento) {
     const rawResponse = await fetch(url, {
@@ -43,16 +43,138 @@ async function sendRequest(nome, cpf, dataNascimento) {
         )
     });
     const content = await rawResponse.json();
-    console.log(nome);
-    console.log(content['mensagem']);
+    //console.log(nome);
+    //console.log(content['mensagem']);
+    //console.log(content)
+    //console.log("_________________________________________")
+
+    global = content
+    var alfaepoetinaAbertura = "-"
+    var alfaepoetinaRenovacao = "-"
+    var sacaratoAbertura = "-"
+    var sacaratoRenovacao = "-"
+    var paricalcitolAbertura = "-"
+    var paricalcitolRenovacao = "-"
+    var cinacalceteAbertura = "-"
+    var cinacalceteRenovacao = "-"
+    var sevelamerAbertura = "-"
+    var sevelamerRenovacao = "-"
+    var calcitriolAbertura = "-"
+    var calcitriolRenovacao = "-"
+    
+    for(var i=7;i>=0;i=i-1){
+        try {
+            //colocar o codido do procedimento referente ao calcitriol e estara pronto
+            if(content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "000" && content["objeto"][i].dscStatus === "Deferido") 
+            {
+                calcitriolAbertura = ( content["objeto"][i].dthAbertura )
+                calcitriolRenovacao = ( content["objeto"][i].dthReenvioAnalise )
+                if(calcitriolRenovacao == ""){
+                    calcitriolRenovacao = "-"
+                }
+                if(calcitriolAbertura == ""){
+                    calcitriolAbertura == "-"
+                }
+                
+            }
+
+            if(content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "195" && content["objeto"][i].dscStatus === "Deferido") 
+            {
+                sevelamerAbertura = ( content["objeto"][i].dthAbertura )
+                sevelamerRenovacao = ( content["objeto"][i].dthReenvioAnalise )
+                if(sevelamerRenovacao == ""){
+                    sevelamerRenovacao = "-"
+                }
+                if(sevelamerAbertura == ""){
+                    sevelamerAbertura == "-"
+                }
+                
+            }
+
+            if(content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "394" && content["objeto"][i].dscStatus === "Deferido" || content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "395" && content["objeto"][i].dscStatus === "Deferido") 
+            {
+                cinacalceteAbertura = ( content["objeto"][i].dthAbertura )
+                cinacalceteRenovacao = ( content["objeto"][i].dthReenvioAnalise )
+                if(cinacalceteRenovacao == ""){
+                    cinacalceteRenovacao = "-"
+                }
+                if(cinacalceteAbertura == ""){
+                    cinacalceteAbertura == "-"
+                }
+                
+            }
+
+            if(content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "221" && content["objeto"][i].dscStatus === "Deferido") 
+            {
+                sacaratoAbertura = ( content["objeto"][i].dthAbertura )
+                sacaratoRenovacao = ( content["objeto"][i].dthReenvioAnalise )
+                if(sacaratoRenovacao == ""){
+                    sacaratoRenovacao = "-"
+                }
+                if(sacaratoAbertura == ""){
+                    sacaratoAbertura == "-"
+                }
+                
+            }
 
 
-    document.getElementById("tabela").innerHTML += "<tr><td>"+nome+"</td><td>    =>   </td><td>"+content['mensagem']+"</td></tr>"
+            if(content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "10" && content["objeto"][i].dscStatus === "Deferido" || content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "11" && content["objeto"][i].dscStatus === "Deferido" || content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "12" && content["objeto"][i].dscStatus === "Deferido") 
+            {
+                alfaepoetinaAbertura = ( content["objeto"][i].dthAbertura )
+                alfaepoetinaRenovacao = ( content["objeto"][i].dthReenvioAnalise )
+                if(alfaepoetinaRenovacao == ""){
+                    alfaepoetinaRenovacao = "-"
+                }
+                if(alfaepoetinaAbertura == ""){
+                    alfaepoetinaAbertura == "-"
+                }
+                
+            }
+
+            if(content["objeto"][i].vetProcedimentoApac[0].codProcedimentoApac === "396" && content["objeto"][i].dscStatus === "Deferido") 
+            {
+                paricalcitolAbertura = ( content["objeto"][i].dthAbertura )
+                paricalcitolRenovacao = ( content["objeto"][i].dthReenvioAnalise )
+                if(paricalcitolRenovacao == ""){
+                    paricalcitolRenovacao = "-"
+                }
+                if(paricalcitolAbertura == ""){
+                    paricalcitolAbertura == "-"
+                }
+                
+            }
+            
+        } catch {
+            console.log('erro')
+            
+        }
+
+        
+
+    }
+
+    
+
+    
+
+
+
+    //document.getElementById("tabela").innerHTML += "<tr><td>"+nome+"</td><td>    =>   </td><td>"+content['mensagem']+"</td></tr>"
+    document.getElementById("dados").innerHTML += 
+    "<tr>\
+        <td>"+nome+"</td>\
+        <td>Abertura:"+alfaepoetinaAbertura+" <br>R: "+alfaepoetinaRenovacao+"</td>\
+        <td>Abertura:"+sacaratoAbertura+" <br>R: "+sacaratoRenovacao+"</td>\
+        <td>Abertura:"+paricalcitolAbertura+" <br>R: "+paricalcitolRenovacao+"</td>\
+        <td>Abertura:"+cinacalceteAbertura+" <br>R: "+cinacalceteRenovacao+"</td>\
+        <td>Abertura:"+sevelamerAbertura+" <br>R: "+sevelamerRenovacao+"</td>\
+        <td>Abertura:"+calcitriolAbertura+" <br>R: "+calcitriolRenovacao+"</td>\
+    </tr>"
 }
 
 
 function start(){
-    token = document.getElementById("key").value;
+    //token = document.getElementById("key").value;
     pacients.forEach(element => {
 
         sendRequest(element['nome'], element['cpf'], element['dataNasc'])
--- pacientes.js
+++ pacientes.js
@@ -1,12 +1,32 @@
 const pacients = [
+    
     {
-        'nome': 'Ludimila Martins De Oliveira',
-        'cpf': '000.999.000-02',
-        'dataNasc': '1995-04-14'
+        'nome': 'Filipe Iego Gomes',
+        'cpf': '012.507.386-01',
+        'dataNasc': '1988-03-25'
     },
     {
-        'nome': 'Marlon Felipe Martins De Oliveira',
-        'cpf': '000.999.000-03',
-        'dataNasc': '1997-06-20'
-    }
+        'nome': 'Fatima Conceicao dos santos',
+        'cpf': '789.217.916-04',
+        'dataNasc': '1962-07-14'
+    },
+    {
+        'nome': 'Gustavo Malaquias de oliveira',
+        'cpf': '097.311.416-93',
+        'dataNasc': '1993-11-19'
+    },
+    {
+        'nome': 'Agnaldo Mendes de oliveira',
+        'cpf': '114.772.936-04',
+        'dataNasc': '1987-04-24'
+    },
+    {
+        'nome': 'altamir da silva lage',
+        'cpf': '243.404.306-25',
+        'dataNasc': '1946-09-29'
+    },
+    
+
+
+    
 ]
